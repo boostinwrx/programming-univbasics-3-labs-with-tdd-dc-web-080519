2310d11e691d43b2bd8e8b4c6c5a6f33028585ae
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-07-20 01:29:07 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,5 @@ first_number = 1
 second_number = 124.12
 sum = nil
 def calculator(first_number, second_number)
-first_number + second_number = sum
-puts sum
+sum = (first_number + second_number)
 end
