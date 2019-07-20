574d67d481ab528163985491498bcc0353aaf6bb
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-07-20 01:16:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,7 @@
+first_number = 5.1
+second_number = 2.2
 
-
-def calculator
+def calculator(first_number, second_number)
   first_number = 5.1
   second_number = 2.2
   first_number+second_number = sum
