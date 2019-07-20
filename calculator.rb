4123536e11ca00ec13b619ee06e3d76a4000857b
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-07-20 01:12:52 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,8 @@
-first_number = 1.1
+first_number = 5.1
 second_number = 2.2
 
 def calculator(first_number, second_number)
   sum = 0
-  first_number + second_number = sum
+  first_number - second_number = sum
   puts sum
 end
